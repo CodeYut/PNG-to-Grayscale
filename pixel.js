fb3bed566c786be195e42ceef6c2823bc746d313
eric phillips <grinder123089@aol.com>
2021-04-25 18:55:21 -0700

    updates

--- pixel.js
+++ pixel.js
@@ -1,1 +1,11 @@
-const canvas = document.getElementById('canvas1')
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 450;
+
+const image1 = new Image();
+image1.src = 'image1.png';
+ctx.drawImage(image1, 0, 0);
+image1.addEventListener('load', function(){
+    ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+})
